5d87ee22b425c0a29f72a9f772b5ecccdd4e6e2c
Popov Philipp <philolippo@gmail.com>
2020-11-01 05:22:04 +0300

    Implement reading headers

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,6 +1,9 @@
 use crate::data_writer;
 use reqwest::blocking::{Client, Response};
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead};
 
 pub struct Crawler {
     pub urls: Vec<String>,
@@ -10,6 +13,8 @@ pub struct Crawler {
     pub parse_function: fn(Response) -> data_writer::Product,
 
     pub client: Client,
+
+    pub headers: HeaderMap,
 }
 
 impl Crawler {
@@ -18,6 +23,7 @@ impl Crawler {
             urls,
             parse_function,
             client: Client::new(),
+            headers: HeaderMap::new(),
         }
     }
 
@@ -31,5 +37,21 @@ impl Crawler {
         let response = self.get_response(url).unwrap();
 
         (self.parse_function)(response)
+    }
+
+    pub fn read_headers(&mut self, filename: &str) {
+        let file = File::open(filename).expect("Failed to read file");
+
+        let lines = io::BufReader::new(file).lines();
+
+        for line in lines {
+            let line = line.unwrap();
+            let mut values: Vec<&str> = line.split(": ").collect();
+            // Pile of sh*t
+            let header_value: HeaderValue = values.pop().unwrap().parse().unwrap();
+            let header_name: HeaderName = values.pop().unwrap().parse().unwrap();
+
+            self.headers.insert(header_name, header_value);
+        }
     }
 }
